801d4f427e3803a9689064c3c7197f4bf492f5a8
jack <chao.li.cse@gmail.com>
2014-12-01 18:56:55 -0800

    removed junks

--- core/lab3/work/cl_state_machine_sv_unit/_primary.vhd
+++
@@ -1,4 +0,0 @@
-library verilog;
-use verilog.vl_types.all;
-entity cl_state_machine_sv_unit is
-end cl_state_machine_sv_unit;
--- core/lab3/work/core_flattened_sv_unit/_primary.vhd
+++
@@ -1,4 +0,0 @@
-library verilog;
-use verilog.vl_types.all;
-entity core_flattened_sv_unit is
-end core_flattened_sv_unit;
